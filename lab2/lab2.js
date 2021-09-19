1b4b11eee360cfe4494106cdbd5f77256bacf00c
Khoma <88534376+danzillo@users.noreply.github.com>
2021-09-19 20:04:23 +0700

    Update and rename lab1.js to lab2.js

---
+++ lab2/lab2.js
@@ -0,0 +1,16 @@
+let obj = { 
+        key1: 'a1', 
+        key2: 'a2', 
+        key3: 'a3', 
+} 
+ 
+var type = typeof(obj); 
+ 
+if (type!="object" || obj==null) 
+ console.log('проверьте введенные данные') 
+else 
+function transform(anyobject){ 
+    var arr = Object.entries(anyobject); 
+} 
+ 
+transform(obj);
